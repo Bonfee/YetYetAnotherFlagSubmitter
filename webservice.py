93e6755049f97425dbfd9d86e2d262d0dd92ab11
Bonfee <53857102+Bonfee@users.noreply.github.com>
2022-06-17 16:40:37 +0200

    Use lpush instead of publish

--- webservice.py
+++ webservice.py
@@ -26,7 +26,7 @@ def submit_many():
 
     if len(valid_flags) > 0:
         for flag in valid_flags:
-            RedisConnection().red.publish(Config.Redis.channel, f"{flag}|{exploit}|1|{target}")
+            RedisConnection().red.lpush(Config.Redis.channel, f"{flag}|{exploit}|1|{target}")
 
 
 @bottle.post('/submit')
@@ -38,7 +38,7 @@ def submit():
 
     # Decide whether to send the matched string or the original flag
     if re.match(Config.Flag.regex, flag):
-        RedisConnection().red.publish(Config.Redis.channel, f"{flag}|{exploit}|1|{target}")
+        RedisConnection().red.lpush(Config.Redis.channel, f"{flag}|{exploit}|1|{target}")
     else:
         print('Regex fail')  # TO DO
         return bottle.HTTPResponse({'error': 'Flag was not correct'}, 400)
